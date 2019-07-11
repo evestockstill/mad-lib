389c954a96676bf654de37907e7775797d0cdb54
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-11 16:03:09 -0700

    redoMadLib

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-let noun
+const nounOneInput = document.getElementById('noun-one');
+const verbInput = document.getElementById('verb');
+const nounTwo = document.getElementById('noun-two');
+const adjective = document.getElementById('adjective');
+const number = document.getElementById('number');
+const numberTwo = document.getElementById('number-2');
+
+const blanks = document.getElementById('mad-lib');
+
+function printMadLib() 
+{
+  nounOneSpan.textContent = nounOneInput.value;
+  verbSpan.textContent = verbInput.value;
+  nounTwoSpan.textContent = nounTwoInput.value;
+  adjectiveSpan.textContent = adjectiveInput.value;
+  numberOneSpan.textContent = numberOneInput.value;
+  numberTwoSpan.textContent = numberTwoInput.value;
+  blanks.classList.remove('hidden');
+}
