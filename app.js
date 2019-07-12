04cf49ae3da4c7e2f6259a5e6c824b507decd32d
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-11 18:10:30 -0700

    anotherMadLib

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const nounOneInput = document.getElementById('noun-one');
-const verbInput = document.getElementById('verb');
-const nounTwo = document.getElementById('noun-two');
-const adjective = document.getElementById('adjective');
-const number = document.getElementById('number');
-const numberTwo = document.getElementById('number-2');
-
+const nounOneInput = document.getElementById('noun-one-input');
+const verbInput = document.getElementById('verb-input');
+const nounTwoInput = document.getElementById('noun-two-input');
+const adjectiveInput = document.getElementById('adjective-input');
+const numberInput = document.getElementById('number-input');
+const numberTwoInput = document.getElementById('number-2-input');
 const blanks = document.getElementById('mad-lib');
-
 function printMadLib() 
 {
-  nounOneSpan.textContent = nounOneInput.value;
+  nounOne.textContent = nounOneInput.value;
   verbSpan.textContent = verbInput.value;
-  nounTwoSpan.textContent = nounTwoInput.value;
-  adjectiveSpan.textContent = adjectiveInput.value;
-  numberOneSpan.textContent = numberOneInput.value;
-  numberTwoSpan.textContent = numberTwoInput.value;
+   nounTwoSpan.textContent = nounTwoInput.value;
+   adjectiveSpan.textContent = adjectiveInput.value;
+   numberOneSpan.textContent = numberOneInput.value;
+   numberTwoSpan.textContent = numberTwoInput.value;
+
   blanks.classList.remove('hidden');
 }
